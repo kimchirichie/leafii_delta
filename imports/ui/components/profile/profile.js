57a84bcaf231eb84e1b425ea320bc28123366038
Talent Zeng <talent.z@hotmail.com>
2016-06-23 14:19:14 -0400

    basic upload function, doesn't work

--- imports/ui/components/profile/profile.js
+++ imports/ui/components/profile/profile.js
@@ -2,12 +2,13 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 import uiRouter from 'angular-ui-router';
+import ngFileUpload from 'ng-file-upload';
  
 import { Accounts } from 'meteor/accounts-base';
 import template from './profile.html';
 
 class Profile {
-  constructor($scope, $reactive, $timeout){
+  constructor($scope, $reactive, $timeout, Upload){
     "ngInject";
     $reactive(this).attach($scope);
     this.getUser();
@@ -15,6 +16,21 @@ class Profile {
 
   getUser(){
     this.user = Meteor.user();
+  }
+
+  uploadPic(file) {
+    console.log('hehaha');
+    Upload.upload({
+      url: 'upload/url',
+      data: {file:file, '_id': Meteor.userId()}
+    }).then(function(resp){
+      console.log('Success ' + resp.config.data.file.name + 'uploaded. Response: ' + resp.data);
+    }, function(resp){
+      console.log('Error status: ' + resp.status);
+    }, function(evt){
+      var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+      console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file.name);
+    });
   }
 
   update(user){
@@ -28,6 +44,7 @@ const name = 'profile';
 export default angular.module(name, [
   angularMeteor,
   uiRouter,
+  ngFileUpload
 ]).component(name, {
   template,
   controllerAs: name,
