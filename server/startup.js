cc2a8a6d4052e3ebd4d426ec1c4bc11b18aea9b3
Krisztian Palvolgyi <kpalvolg@uwaterloo.ca>
2016-08-16 16:59:55 -0400

    Added url and name to posts

--- server/startup.js
+++ server/startup.js
@@ -151,12 +151,11 @@ Meteor.startup(()=>{
 			}
 		},
 
-		createPost(title, tags, content, url){
+		createPost(title, tags, content){
 			if(Meteor.userId()){
-				user = Meteor.userId();
+				user = Meteor.user();
 				date = Date.now();
-
-				Posts.insert({poster_user_id: user, title: title, tags: tags, content: content, url: url, comments: [], date: date, last_edit: 0});
+				Posts.insert({poster_user_id: user._id, title: title, tags: tags, content: content, url: user.profile.url, name: user.profile.firstName + " " + user.profile.lastName, comments: [], date: date, last_edit: 0});
 
 				console.log(Posts.find({}).fetch());
 			}
