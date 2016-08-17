adcec87ae92bec1c7a9b8c4c42f5fb0d75a0d6f6
Richard Sin <sinr0202@gmail.com>
2016-08-16 22:41:16 -0400

    fixed the scss hanging glitch & touched up on admin console

--- imports/api/users/index.js
+++ imports/api/users/index.js
@@ -4,8 +4,31 @@ if (Meteor.isServer) {
 	Meteor.publish('allUsers', function(){
 		return Meteor.users.find({},{fields: {profile:1, createdAt: 1}});
 	});
-	Meteor.publish('potato', function(){
-		return Meteor.users.find({},{});
-	})
 
+	Meteor.publish('potato', function(){
+		const user = Meteor.users.findOne(this.userId);
+		if(user.profile.role == "admin"){
+			return Meteor.users.find({});
+		} else {
+			return this.ready();
+		}
+	});
+
+	Meteor.users.allow({
+		insert(userId, doc) {
+			const user = Meteor.users.findOne({_id: userId});
+			console.log(user);
+			return user.profile.role == "admin";
+		},
+		update(userId, doc, fields, modifier){
+			const user = Meteor.users.findOne({_id: userId});
+			console.log(user);
+			return user.profile.role == "admin";	
+		},
+		remove(userId, doc){
+			const user = Meteor.users.findOne({_id: userId});
+			console.log(user.profile);
+			return user.profile.role == "admin";
+		}
+	});
 }
